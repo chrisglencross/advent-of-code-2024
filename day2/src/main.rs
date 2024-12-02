7cf10508f22989e1b35853f7e36dd5a819bfc37a
Chris Glencross <chris@glencross.org>
2024-12-02 09:18:31 +0000

    Day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,22 +1,38 @@
-use regex::Regex;
+use std::iter::zip;
 
 const DAY: u8 = 2;
 
 fn main() {
-    let input = aocutil::load_test_input(DAY);
-    let _ = parse_input(&input);
+    let input = aocutil::load_input(DAY);
+    let rows = parse_input(&input);
 
-    let part1 : i64 = 0; // TODO
+    let part1 = rows.iter()
+        .filter(|r| is_safe(r))
+        .count();
     println!("Part 1: {part1}");
 
-    let part2 : i64 = 0; // TODO
+    let part2 = rows.iter().filter(|r| is_safe_with_dampener(r)).count();
     println!("Part 2: {part2}");
 }
 
-fn parse_input(input: &str) -> () {
-    let re = Regex::new(r"^(\d+) +(\d+)$").unwrap();
-    for line in input.lines() {
-        let (_, [c1, c2]) = re.captures(line).unwrap().extract();
-    }
-    ()
+fn is_safe(row: &Vec<i64>) -> bool {
+    return (row.iter().is_sorted() || row.iter().rev().is_sorted()) &&
+        zip(&row[0..row.len() - 1], &row[1..])
+            .all(|(&n0, &n1)| (1..=3).contains(&(n1 - n0).abs()));
+}
+
+fn is_safe_with_dampener(row: &Vec<i64>) -> bool {
+    is_safe(row) || (0..row.len())
+        .map(|remove_index| row.iter().enumerate()
+            .filter_map(|(i, r)| if i == remove_index { None } else { Some(*r) })
+            .collect())
+        .any(|dampened_row| is_safe(&dampened_row))
+}
+
+fn parse_input(input: &str) -> Vec<Vec<i64>> {
+    input.lines().map(|line| -> Vec<i64> {
+        line.split_whitespace()
+            .map(|s| s.parse().unwrap())
+            .collect()
+    }).collect()
 }

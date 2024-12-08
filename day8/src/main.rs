2feee884013c2db3257d6b8abf1a2176b611aec4
Chris Glencross <chris@glencross.org>
2024-12-08 09:44:51 +0000

    Day 8

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -14,40 +14,40 @@ fn main() {
     let grid = Grid::parse(&input);
 
     let masts = grid.index_repeating_cells("", ".");
-    let all_coords: HashSet<Coord> = grid.all_coords().iter().map(|&c| c.clone()).collect();
+    let grid_coords: HashSet<Coord> = grid.all_coords().iter().map(|&c| *c).collect();
 
-    let part1 = count_locations(&masts, locations_part_1, &all_coords);
+    let part1 = count_locations(&masts, locations_part_1, &grid_coords);
     println!("Part 1: {part1}");
 
-    let part2 = count_locations(&masts, locations_part_2, &all_coords);
+    let part2 = count_locations(&masts, locations_part_2, &grid_coords);
     println!("Part 2: {part2}");
 }
 
 fn count_locations(masts: &HashMap<char, Vec<Coord>>,
                    antinode_locations: fn(Coord, Coord, &HashSet<Coord>) -> Vec<(i64, i64)>,
-                   all_coords: &HashSet<Coord>) -> usize {
+                   grid_coords: &HashSet<Coord>) -> usize {
     masts.values()
         .flat_map(|coords| coords.iter().permutations(2)
             .map(|pair| (pair[0], pair[1]))
-            .flat_map(|(&c0, &c1)| antinode_locations(c0, c1, all_coords))
+            .flat_map(|(&c0, &c1)| antinode_locations(c0, c1, grid_coords))
         )
         .unique()
-        .filter(|&c| all_coords.contains(&c))
+        .filter(|c| grid_coords.contains(c))
         .count()
 }
 
-fn locations_part_1(a: Coord, b: Coord, _all_coords: &HashSet<Coord>) -> Vec<(i64, i64)> {
+fn locations_part_1(a: Coord, b: Coord, _grid_coords: &HashSet<Coord>) -> Vec<(i64, i64)> {
     let d = coord::sub(b, a);
     [coord::sub(a, d), coord::add(b, d)].iter()
         .map(|&c| c)
         .collect()
 }
 
-fn locations_part_2(a: Coord, b: Coord, all_coords: &HashSet<Coord>) -> Vec<(i64, i64)> {
+fn locations_part_2(a: Coord, b: Coord, grid_coords: &HashSet<Coord>) -> Vec<(i64, i64)> {
     let d = coord::sub(b, a);
     (0..)
         .map(|i| [coord::sub(a, coord::mul(d, i)), coord::add(b, coord::mul(d, i))])
-        .take_while(|cs| cs.iter().any(|c| all_coords.contains(c)))
+        .take_while(|cs| cs.iter().any(|c| grid_coords.contains(c)))
         .flat_map(|cs| cs)
         .collect()
 }

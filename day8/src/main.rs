ff5ce658658c3c48aa3192ccbdf01953f6dab2a5
Chris Glencross <chris@glencross.org>
2024-12-08 21:16:53 +0000

    Minor tweak to grid API

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use itertools;
 use itertools::Itertools;
 
@@ -13,37 +11,35 @@ fn main() {
     let input = aocutil::load_input(DAY);
     let grid = Grid::parse(&input);
 
-    let masts = grid.index_repeating_cells("", ".");
-
-    let part1 = count_locations(&masts, locations_part_1, &grid);
+    let part1 = count_locations(locations_for_mast_pair_pt1, &grid);
     println!("Part 1: {part1}");
 
-    let part2 = count_locations(&masts, locations_part_2, &grid);
+    let part2 = count_locations(locations_for_mast_pair_pt2, &grid);
     println!("Part 2: {part2}");
 }
 
-fn count_locations(masts: &HashMap<char, Vec<Coord>>,
-                   antinode_locations: fn(Coord, Coord, &Grid) -> Vec<Coord>,
+fn count_locations(locations_for_mast_pair: fn(Coord, Coord, &Grid) -> Vec<Coord>,
                    grid: &Grid) -> usize {
+    let masts = grid.index_repeating_cells("", ".");
     masts.values()
         .flat_map(|coords| coords.iter()
             .permutations(2)
             .map(|pair| (pair[0], pair[1]))
-            .flat_map(|(&c0, &c1)| antinode_locations(c0, c1, grid))
+            .flat_map(|(&c0, &c1)| locations_for_mast_pair(c0, c1, grid))
         )
         .unique()
         .filter(|&c| grid.contains_coord(c))
         .count()
 }
 
-fn locations_part_1(a: Coord, b: Coord, _grid: &Grid) -> Vec<Coord> {
+fn locations_for_mast_pair_pt1(a: Coord, b: Coord, _grid: &Grid) -> Vec<Coord> {
     let d = coord::sub(b, a);
     [coord::sub(a, d), coord::add(b, d)].iter()
         .map(|&c| c)
         .collect()
 }
 
-fn locations_part_2(a: Coord, b: Coord, grid: &Grid) -> Vec<Coord> {
+fn locations_for_mast_pair_pt2(a: Coord, b: Coord, grid: &Grid) -> Vec<Coord> {
     let d = coord::sub(b, a);
     (0..)
         .map(|i| [coord::sub(a, coord::mul(d, i)), coord::add(b, coord::mul(d, i))])

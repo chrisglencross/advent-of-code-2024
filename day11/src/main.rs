56e93de270b6dee3e98aeb56cbf5470e9eb967b6
Chris Glencross <chris@glencross.org>
2024-12-11 12:16:05 +0000

    Day 11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
 
 fn length_after_iterations(values: &Vec<i64>, iterations: i64, cache: &mut HashMap<(i64, i64), usize>) -> usize {
     values.iter()
-        .map(|&next| length_after_iterations1(next, iterations - 1, cache))
+        .map(|&value| length_after_iterations1(value, iterations - 1, cache))
         .sum()
 }
 
@@ -32,16 +32,16 @@ fn length_after_iterations1(value: i64, iterations: i64, cache: &mut HashMap<(i6
     }
 }
 
-fn expand(v: i64) -> Vec<i64> {
-    if v == 0i64 {
+fn expand(value: i64) -> Vec<i64> {
+    if value == 0i64 {
         vec![1]
     } else {
-        let digits = v.ilog10() + 1;
+        let digits = value.ilog10() + 1;
         if digits % 2 == 0 {
             let split = 10i64.pow(digits / 2);
-            vec![v / split, v % split]
+            vec![value / split, value % split]
         } else {
-            vec![v * 2024]
+            vec![value * 2024]
         }
     }
 }

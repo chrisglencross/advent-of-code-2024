34f5a1ca0797859121295a859954d52881023ffb
Chris Glencross <chris@glencross.org>
2024-12-18 12:29:53 +0000

    Day 18 switch to using a pathfinding library

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,30 +1,10 @@
-use std::cmp::Ordering;
-use std::collections::{BinaryHeap, HashMap};
 use regex::Regex;
 use aocutil::coord::Coord;
-use aocutil::direction::{COMPASS, Direction, Directions};
+use aocutil::direction::{COMPASS, Directions};
 use aocutil::grid::Grid;
+use pathfinding::prelude::bfs;
 
 const DAY: u8 = 18;
-
-#[derive(Clone, Eq, PartialEq)]
-struct Node {
-    position: Coord,
-    ticks: i64,
-    path: Vec<Coord>,
-}
-
-impl Ord for Node {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.ticks.cmp(&self.ticks)
-    }
-}
-
-impl PartialOrd for Node {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
 
 fn main() {
     let input = aocutil::load_input(DAY);
@@ -36,19 +16,19 @@ fn main() {
 
     let mut grid = Grid::new_with_coords(byte_coords.iter().take(part1_ticks), '#');
     let part1= shortest_route(&grid, start, end).unwrap();
-    println!("Part 1: {}", part1.ticks);
+    println!("Part 1: {}", part1.len());
 
     let part2 = add_coords_until_no_route(&mut grid, start, end, byte_coords.clone().into_iter().skip(part1_ticks).collect());
     println!("Part 2: {},{}", part2.0, part2.1);
 }
 
 fn add_coords_until_no_route(grid: &mut Grid, start: Coord, end: Coord, remaining_coords: Vec<Coord>) -> Coord {
-    let mut current_route = shortest_route(&grid, start, end).unwrap().path;
+    let mut current_route = shortest_route(&grid, start, end).unwrap();
     for coord in remaining_coords {
         grid.set(coord, '#');
         if current_route.contains(&coord) {
             match shortest_route(&grid, (0, 0), end) {
-                Some(node) => current_route = node.path,
+                Some(route) => current_route = route,
                 None => return coord
             }
         }
@@ -56,59 +36,15 @@ fn add_coords_until_no_route(grid: &mut Grid, start: Coord, end: Coord, remainin
     panic!()
 }
 
-fn shortest_route(grid: &Grid, start: Coord, end: Coord) -> Option<Node> {
-    let mut best_scores: HashMap<Coord, i64> = HashMap::new();
-
-    let mut priority_queue: BinaryHeap<Node> = BinaryHeap::new();
-    priority_queue.push(Node { position: start, ticks: 0, path: vec![start] });
-
-    let mut best_solution: Option<Node> = None;
-    while let Some(node) = priority_queue.pop() {
-        if best_solution.as_ref().map(|best_solution| best_solution.ticks < node.ticks).unwrap_or(false) {
-            continue;
-        } else if node.position == end {
-            best_solution = Some(node);
-        } else {
-            let next_nodes: Vec<Node> = get_next_nodes(&grid, &node).into_iter()
-                .filter(|n| is_best_score(&best_scores, &best_solution, n))
-                .collect();
-            for &Node { position, ticks, .. } in &next_nodes {
-                best_scores.insert(position, ticks);
-            }
-            priority_queue.extend(next_nodes);
-        }
-    }
-
-    best_solution
+fn shortest_route(grid: &Grid, start: Coord, end: Coord) -> Option<Vec<Coord>> {
+    bfs(&start, |p| get_next_nodes(grid, *p), |p| *p == end)
 }
 
-fn is_best_score(best_scores: &HashMap<Coord, i64>, best_solution: &Option<Node>, node: &Node) -> bool {
-    if !&best_solution.is_none() && node.ticks > best_solution.as_ref().map(|b| b.ticks).unwrap() {
-        false
-    } else {
-        match best_scores.get(&node.position) {
-            None => true,
-            Some(&best_score) => node.ticks < best_score
-        }
-    }
-}
-
-fn get_next_nodes(grid: &Grid, node: &Node) -> Vec<Node> {
-    let mut result = vec![];
-    let coord = node.position;
-    for direction in COMPASS.directions() {
-        if can_step(grid, coord, direction) {
-            let next_coord = direction.step(coord);
-            let mut new_path = node.path.clone();
-            new_path.push(next_coord);
-            result.push(Node { position: next_coord, ticks: node.ticks + 1, path: new_path });
-        }
-    }
-    result
-}
-
-fn can_step(grid: &Grid, coord: Coord, direction: &Direction) -> bool {
-    grid.is_in_bounds(coord) && grid.get_or(direction.step(coord), '.') != '#'
+fn get_next_nodes(grid: &Grid, coord: Coord) -> Vec<Coord> {
+    COMPASS.directions().iter()
+        .map(|d| d.step(coord))
+        .filter(|&c| grid.is_in_bounds(c) && grid.get_or(c, '.') != '#')
+        .collect()
 }
 
 fn parse_input(input: &str) -> Vec<Coord> {

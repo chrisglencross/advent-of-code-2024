ca86567f6ecdcb77f2d11df051a5f084f2e223d4
Chris Glencross <chris@glencross.org>
2024-12-19 07:51:10 +0000

    Day 19

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::HashMap;
 
 const DAY: u8 = 19;
 
@@ -6,36 +6,35 @@ fn main() {
     let input = aocutil::load_input(DAY);
     let (patterns, designs) = parse_input(&input);
 
-    let mut negative_cache = HashSet::new();
+    let mut cache = HashMap::new();
     let part1 = designs.iter()
-        .filter(|d| is_possible(&patterns, d, &mut negative_cache))
+        .filter(|d| count_possible(&patterns, d, &mut cache) > 0)
         .count();
     println!("Part 1: {part1}");
 
-    let part2 : i64 = 0; // TODO
+    let part2: i64 = designs.iter()
+        .map(|d| count_possible(&patterns, d, &mut cache))
+        .sum();
     println!("Part 2: {part2}");
 }
 
-fn is_possible<'a>(patterns: &Vec<&str>, design: &'a str, negative_cache: &mut HashSet<&'a str>) -> bool {
-    if negative_cache.contains(design) {
-        false
-    } else if design.is_empty() {
-        true
+fn count_possible<'a>(patterns: &Vec<&str>, design: &'a str, cache: &mut HashMap<&'a str, i64>) -> i64 {
+    if design.is_empty() {
+        1
+    } else if let Some(&cached_value) = cache.get(design) {
+        cached_value
     } else {
-        for pattern in patterns {
-            if design.starts_with(pattern) && is_possible(patterns, &design[pattern.len()..], negative_cache) {
-                return true
-            }
-        }
-        negative_cache.insert(design);
-        false
+        let value: i64 = patterns.iter()
+            .filter(|&pattern| design.starts_with(pattern))
+            .map(|pattern| count_possible(patterns, &design[pattern.len()..], cache))
+            .sum();
+        cache.insert(design, value);
+        value
     }
 }
 
 
 fn parse_input(input: &str) -> (Vec<&str>, Vec<&str>) {
     let (block1, block2) = input.split_once("\n\n").unwrap();
-    (
-        block1.split(", ").collect(),
-        block2.lines().collect())
+    (block1.split(", ").collect(), block2.lines().collect())
 }

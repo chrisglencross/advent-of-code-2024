50850598a60afe739d09dbaa959964e388ad3ed1
Chris Glencross <chris@glencross.org>
2024-12-20 09:15:57 +0000

    Day 20

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,7 +1,3 @@
-use std::collections::HashMap;
-
-use itertools;
-use itertools::Itertools;
 use pathfinding::prelude::bfs;
 
 use aocutil::coord::{Coord, manhattan_distance};
@@ -17,23 +13,22 @@ fn main() {
     let start = grid.find_cell('S').unwrap();
     let end = grid.find_cell('E').unwrap();
 
-    let baseline = shortest_route(&grid, start, end);
-    let route_distances_from_start: HashMap<Coord, i64> = baseline.iter()
-        .enumerate()
-        .map(|(distance_from_start, coord)| (*coord, distance_from_start as i64))
-        .collect();
-
-    println!("Part 1: {}", cheats_saving_at_least_100(&route_distances_from_start, 2));
-    println!("Part 2: {}", cheats_saving_at_least_100(&route_distances_from_start, 20));
+    let route = shortest_route(&grid, start, end);
+    println!("Part 1: {}", cheats_saving_at_least_100(&route, 2));
+    println!("Part 2: {}", cheats_saving_at_least_100(&route, 20));
 }
 
-fn cheats_saving_at_least_100(distances_from_start: &HashMap<Coord, i64>, max_shortcut_distance: i64) -> usize {
-    distances_from_start.iter().permutations(2)
-        .map(|pair| (pair[0], pair[1]))
-        .map(|((&c0, &d0), (&c1, &d1))| (d1 - d0, manhattan_distance(c0, c1)))
-        .filter(|&(_route_distance, shortcut_distance)| shortcut_distance <= max_shortcut_distance)
-        .filter(|&(route_distance, shortcut_distance)| route_distance - shortcut_distance >= 100)
-        .count()
+fn cheats_saving_at_least_100(route: &Vec<Coord>, max_shortcut_distance: i64) -> i64 {
+    let mut count = 0;
+    for (d0, &c0) in route.iter().enumerate() {
+        for (route_distance, &c1) in route[d0..].iter().enumerate() {
+            let shortcut_distance = manhattan_distance(c0, c1);
+            if shortcut_distance <= max_shortcut_distance && route_distance as i64 - shortcut_distance >= 100 {
+                count += 1;
+            }
+        }
+    }
+    count
 }
 
 fn shortest_route(grid: &Grid, start: Coord, end: Coord) -> Vec<Coord> {

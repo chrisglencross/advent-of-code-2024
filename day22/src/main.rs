0aa163811f69ca82fffd06780e114150e6dc1392
Chris Glencross <chris@glencross.org>
2024-12-22 12:40:20 +0000

    Day 22

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -4,11 +4,28 @@ const DAY: u8 = 22;
 
 type DeltaPattern = (i64, i64, i64, i64);
 
+struct RandomSequence(i64);
+
+impl Iterator for RandomSequence {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let n = self.0;
+        self.0 = ((self.0 * 64) ^ self.0) % 16777216;
+        self.0 = ((self.0 / 32) ^ self.0) % 16777216;
+        self.0 = ((self.0 * 2048) ^ self.0) % 16777216;
+        Some(n)
+    }
+}
+
 fn main() {
     let input = aocutil::load_input(DAY);
     let values = parse_input(&input);
 
-    println!("Part 1: {}", values.iter().map(|&n| sequence(n)[2000]).sum::<i64>());
+    let part1: i64 = values.iter()
+        .flat_map(|&n| RandomSequence(n).skip(2000).take(1))
+        .sum();
+    println!("Part 1: {}", part1);
 
     let total_pattern_scores = values.iter()
         .map(|&n| pattern_prices_for_monkey(n))
@@ -17,33 +34,18 @@ fn main() {
 }
 
 fn accumulate(mut acc: HashMap<DeltaPattern, i64>, n: &HashMap<DeltaPattern, i64>) -> HashMap<DeltaPattern, i64> {
-    for (&pattern, &price) in n {
-        *acc.entry(pattern).or_default() += price;
-    }
+    n.iter().for_each(|(&pattern, &price)| *acc.entry(pattern).or_default() += price);
     acc
 }
 
 fn pattern_prices_for_monkey(seed: i64) -> HashMap<DeltaPattern, i64> {
-    let mut result: HashMap<DeltaPattern, i64> = HashMap::new();
-    let prices: Vec<i64> = sequence(seed).iter().map(|n| n % 10).collect();
+    let prices: Vec<i64> = RandomSequence(seed).take(2001)
+        .map(|n| n % 10)
+        .collect();
     prices.windows(5)
         .map(|p| ((p[1] - p[0], p[2] - p[1], p[3] - p[2], p[4] - p[3]), p[4]))
-        .for_each(|(pattern, price)| {
-            if !result.contains_key(&pattern) {
-                result.insert(pattern, price);
-            }
-        });
-    result
-}
-fn sequence(seed: i64) -> Vec<i64> {
-    (0..2000).fold(vec![seed], |mut v, _| { v.push(next(v[v.len()-1])); v})
-}
-
-fn next(mut n: i64) -> i64 {
-    n = ((n * 64) ^ n) % 16777216;
-    n = ((n / 32) ^ n) % 16777216;
-    n = ((n * 2048) ^ n) % 16777216;
-    n
+        .rev()  // include earliest prices in HashMap
+        .collect()
 }
 
 fn parse_input(input: &str) -> Vec<i64> {

f736e6fe39c058522d6a4a1c71919ed9e73265da
Chris Glencross <chris@glencross.org>
2024-12-22 09:49:51 +0000

    Day 22

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -2,61 +2,48 @@ use std::collections::HashMap;
 
 const DAY: u8 = 22;
 
-type ChangePattern = (i64, i64, i64, i64);
+type DeltaPattern = (i64, i64, i64, i64);
 
 fn main() {
     let input = aocutil::load_input(DAY);
     let values = parse_input(&input);
 
-    println!("Part 1: {}", values.iter().map(|&n| sequence(n)[2000]).sum());
+    println!("Part 1: {}", values.iter().map(|&n| sequence(n)[2000]).sum::<i64>());
 
     let total_pattern_scores = values.iter()
-        .map(|&n| pattern_scores_for_monkey(n))
+        .map(|&n| pattern_prices_for_monkey(n))
         .fold(HashMap::new(), |acc, m| accumulate(acc, &m));
     println!("Part 2: {}", *total_pattern_scores.values().max().unwrap());
 }
 
-fn accumulate(mut acc: HashMap<ChangePattern, i64>, n: &HashMap<ChangePattern, i64>) -> HashMap<ChangePattern, i64> {
-    for (&key, &value) in n {
-        *acc.entry(key).or_default() += value;
+fn accumulate(mut acc: HashMap<DeltaPattern, i64>, n: &HashMap<DeltaPattern, i64>) -> HashMap<DeltaPattern, i64> {
+    for (&pattern, &price) in n {
+        *acc.entry(pattern).or_default() += price;
     }
     acc
 }
 
-fn pattern_scores_for_monkey(n: i64) -> HashMap<ChangePattern, i64> {
-    let prices = sequence_mod_10(n);
-    let mut result: HashMap<ChangePattern, i64> = HashMap::new();
-    let delta_prices: Vec<(ChangePattern, i64)> = prices.windows(5)
-        .map(|w| ((w[1] - w[0], w[2] - w[1], w[3] - w[2], w[4] - w[3]), w[4]))
-        .collect();
-
-    for (key, value) in delta_prices {
-        if !result.contains_key(&key) {
-            result.insert(key, value);
-        }
-    }
+fn pattern_prices_for_monkey(seed: i64) -> HashMap<DeltaPattern, i64> {
+    let mut result: HashMap<DeltaPattern, i64> = HashMap::new();
+    let prices: Vec<i64> = sequence(seed).iter().map(|n| n % 10).collect();
+    prices.windows(5)
+        .map(|p| ((p[1] - p[0], p[2] - p[1], p[3] - p[2], p[4] - p[3]), p[4]))
+        .for_each(|(pattern, price)| {
+            if !result.contains_key(&pattern) {
+                result.insert(pattern, price);
+            }
+        });
     result
 }
-
-fn sequence_mod_10(n: i64) -> Vec<i64> {
-    sequence(n).iter().map(|n| n % 10).collect()
+fn sequence(seed: i64) -> Vec<i64> {
+    (0..2000).fold(vec![seed], |mut v, _| { v.push(next(v[v.len()-1])); v})
 }
 
-fn sequence(mut n: i64) -> Vec<i64> {
-    let mut result = Vec::with_capacity(2000);
-    result.push(n);
-    for _ in 0..2000 {
-        n = next(n);
-        result.push(n)
-    }
-    result
-}
-
-fn next(n: i64) -> i64 {
-    let a = ((n * 64) ^ n) % 16777216;
-    let b = ((a / 32) ^ a) % 16777216;
-    let c = ((b * 2048) ^ b) % 16777216;
-    c
+fn next(mut n: i64) -> i64 {
+    n = ((n * 64) ^ n) % 16777216;
+    n = ((n / 32) ^ n) % 16777216;
+    n = ((n * 2048) ^ n) % 16777216;
+    n
 }
 
 fn parse_input(input: &str) -> Vec<i64> {

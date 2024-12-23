c30903ac3f31564def6edea5b795a70d929f7201
Chris Glencross <chris@glencross.org>
2024-12-23 21:29:34 +0000

    use std::iter::successors to create the iterator.

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -4,26 +4,12 @@ const DAY: u8 = 22;
 
 type DeltaPattern = (i64, i64, i64, i64);
 
-struct RandomSequence(i64);
-
-impl Iterator for RandomSequence {
-    type Item = i64;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let n = self.0;
-        self.0 = ((self.0 * 64) ^ self.0) % 16777216;
-        self.0 = ((self.0 / 32) ^ self.0) % 16777216;
-        self.0 = ((self.0 * 2048) ^ self.0) % 16777216;
-        Some(n)
-    }
-}
-
 fn main() {
     let input = aocutil::load_input(DAY);
     let values = parse_input(&input);
 
     let part1: i64 = values.iter()
-        .flat_map(|&n| RandomSequence(n).skip(2000).take(1))
+        .flat_map(|&n| random_sequence(n).skip(2000).take(1))
         .sum();
     println!("Part 1: {}", part1);
 
@@ -39,13 +25,25 @@ fn accumulate(mut acc: HashMap<DeltaPattern, i64>, n: &HashMap<DeltaPattern, i64
 }
 
 fn pattern_prices_for_monkey(seed: i64) -> HashMap<DeltaPattern, i64> {
-    let prices: Vec<i64> = RandomSequence(seed).take(2001)
+    let prices: Vec<i64> = random_sequence(seed).take(2001)
         .map(|n| n % 10)
         .collect();
     prices.windows(5)
         .map(|p| ((p[1] - p[0], p[2] - p[1], p[3] - p[2], p[4] - p[3]), p[4]))
         .rev()  // include earliest prices in HashMap
         .collect()
+}
+
+fn random_sequence(seed: i64) -> impl Iterator<Item = i64> {
+    std::iter::successors(
+        Some(seed),
+        move |&n| {
+            let n = ((n * 64) ^ n) % 16777216;
+            let n = ((n / 32) ^ n) % 16777216;
+            let n = ((n * 2048) ^ n) % 16777216;
+            Some(n)
+        },
+    )
 }
 
 fn parse_input(input: &str) -> Vec<i64> {

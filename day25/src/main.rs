05cdf1019918cc187c2cb8f44697166a0387fdac
Chris Glencross <chris@glencross.org>
2024-12-25 11:01:33 +0000

    Day 25

--- day25/src/main.rs
+++ day25/src/main.rs
@@ -1,22 +1,49 @@
-use regex::Regex;
+use aocutil::grid::Grid;
+use itertools;
+use itertools::iproduct;
 
 const DAY: u8 = 25;
 
 fn main() {
-    let input = aocutil::load_test_input(DAY);
-    let _ = parse_input(&input);
+    let input = aocutil::load_input(DAY);
+    let (locks, keys) = parse_input(&input);
 
-    let part1 : i64 = 0; // TODO
+    let part1 = iproduct!(locks, keys)
+        .filter(|(lock, key)| is_compatible(lock, key))
+        .count();
     println!("Part 1: {part1}");
-
-    let part2 : i64 = 0; // TODO
-    println!("Part 2: {part2}");
 }
 
-fn parse_input(input: &str) -> () {
-    // let re = Regex::new(r"^(\d+) +(\d+)$").unwrap();
-    // for line in input.lines() {
-    //     let (_, [c1, c2]) = re.captures(line).unwrap().extract();
-    //
-    // }
+fn is_compatible(lock: &Vec<i64>, key: &Vec<i64>) -> bool {
+    lock.iter().zip(key).all(|(a, b)| a + b <= 5)
+}
+
+fn parse_input(input: &str) -> (Vec<Vec<i64>>, Vec<Vec<i64>>) {
+    let grids: Vec<Grid> = input.split("\n\n")
+        .map(|b| Grid::parse(b))
+        .collect();
+
+    let locks = grids.iter()
+        .filter(|grid| is_lock(grid))
+        .map(|grid|  (0..grid.get_width())
+            .map(|x| (0..grid.get_height())
+                .filter(|&y| grid.get((x, y)).unwrap() == '#')
+                .max().unwrap())
+            .collect())
+        .collect();
+
+    let keys = grids.iter()
+        .filter(|grid| !is_lock(grid))
+        .map(|grid| (0..grid.get_width())
+            .map(|x| (0..grid.get_height())
+                .filter(|&y| grid.get((x, grid.get_height() - y - 1)).unwrap() == '#')
+                .max().unwrap())
+            .collect())
+        .collect();
+
+    (locks, keys)
+}
+
+fn is_lock(grid: &Grid) -> bool {
+    (0..grid.get_width()).all(|x| grid.get((x, 0)).unwrap() == '#')
 }
